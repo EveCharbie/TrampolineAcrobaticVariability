4bdc827b904a456dc956f592ca346c379aef4c7f
Mathieu <math6060@hotmail.fr>
2024-03-11 09:52:13 -0100

    new method is better than the old one

--- get_Q_from_RotMat_new_method.py
+++ get_Q_from_RotMat_new_method.py
@@ -15,7 +15,7 @@ file_path_c3d = "/home/lim/Documents/StageMathieu/DataTrampo/Sarah/Tests/"
 folder_path = "/home/lim/Documents/StageMathieu/DataTrampo/Sarah/"
 
 file_intervals = [
-    # (file_path_c3d + "Sa_bras_volant_1.c3d", (3349, 3850)),
+    # (file_path_c3d + "Sa_bras_volant_1.c3d", (3349, 3950)),
     # (file_path_c3d + "Sa_821_seul_2.c3d", (3431, 3736)),
     (file_path_c3d + "Sa_831_831_6.c3d", (4710, 5009)),
 
@@ -50,14 +50,56 @@ for i in range(nb_mat):
 movement_mat = results_list[0]
 
 for i_frame in range(nb_frames):
-    RotMat_between_segment = []
+    RotMat_between_total = []
     for i_segment in range(nb_mat):
-        RotMat = relax_matrix[i_segment, :, :]
+        RotMat = relax_matrix[i_segment, :, :]  # utile ????
         RotMat_current = movement_mat[i_segment, i_frame, :, :]
         check_matrix_orthogonality(RotMat, "RotMat")
         check_matrix_orthogonality(RotMat_current, "RotMat_current")
 
-        RotMat_between = np.linalg.inv(RotMat) @ RotMat_current
+        index_to_key = {i: key for i, key in enumerate(parent_list.keys())}
+        key_for_given_index = index_to_key[i_segment]
+        info_for_given_index = parent_list[key_for_given_index]
+
+        if info_for_given_index is not None:
+            parent_index, parent_name = info_for_given_index
+            RotMat_between_relax = np.linalg.inv(relax_matrix[parent_index]) @ relax_matrix[i_segment]
+            RotMat_between_relax = biorbd.Rotation(
+                RotMat_between_relax[0, 0],
+                RotMat_between_relax[0, 1],
+                RotMat_between_relax[0, 2],
+                RotMat_between_relax[1, 0],
+                RotMat_between_relax[1, 1],
+                RotMat_between_relax[1, 2],
+                RotMat_between_relax[2, 0],
+                RotMat_between_relax[2, 1],
+                RotMat_between_relax[2, 2],
+            )
+        else:
+            RotMat_between_relax = relax_matrix[i_segment]
+
+        if info_for_given_index is not None:
+            parent_index, parent_name = info_for_given_index
+            RotMat_between_mvt = np.linalg.inv(movement_mat[parent_index, i_frame]) @ movement_mat[i_segment, i_frame]
+            RotMat_between_mvt = biorbd.Rotation(
+                RotMat_between_mvt[0, 0],
+                RotMat_between_mvt[0, 1],
+                RotMat_between_mvt[0, 2],
+                RotMat_between_mvt[1, 0],
+                RotMat_between_mvt[1, 1],
+                RotMat_between_mvt[1, 2],
+                RotMat_between_mvt[2, 0],
+                RotMat_between_mvt[2, 1],
+                RotMat_between_mvt[2, 2],
+            )
+        else:
+            RotMat_between_mvt = movement_mat[i_segment, i_frame]
+
+        if info_for_given_index is not None:
+            RotMat_between_relax = RotMat_between_relax.to_array()
+            RotMat_between_mvt = RotMat_between_mvt.to_array()
+
+        RotMat_between = np.linalg.inv(RotMat_between_relax) @ RotMat_between_mvt
         RotMat_between = biorbd.Rotation(
             RotMat_between[0, 0],
             RotMat_between[0, 1],
@@ -69,30 +111,9 @@ for i_frame in range(nb_frames):
             RotMat_between[2, 1],
             RotMat_between[2, 2],
         )
-        RotMat_between_segment.append(RotMat_between)
+        RotMat_between_total.append(RotMat_between)
 
-    for i_segment in range(nb_mat):
-        index_to_key = {i: key for i, key in enumerate(parent_list.keys())}
-        key_for_given_index = index_to_key[i_segment]
-        info_for_given_index = parent_list[key_for_given_index]
-        if info_for_given_index is not None:
-            parent_index, parent_name = info_for_given_index
-            RotMat_between = np.linalg.inv(RotMat_between_segment[parent_index].to_array()) @ RotMat_between_segment[i_segment].to_array()
-            RotMat_between = biorbd.Rotation(
-                RotMat_between[0, 0],
-                RotMat_between[0, 1],
-                RotMat_between[0, 2],
-                RotMat_between[1, 0],
-                RotMat_between[1, 1],
-                RotMat_between[1, 2],
-                RotMat_between[2, 0],
-                RotMat_between[2, 1],
-                RotMat_between[2, 2],
-            )
-        else:
-            RotMat_between = RotMat_between_segment[i_segment]
-
-        if i_segment in (4, 5, 7, 8, 11, 14):
+        if i_segment in (5, 8, 11, 14):
             Q[i_segment * 3: (i_segment + 1) * 3-1, i_frame] = biorbd.Rotation.toEulerAngles(
                 RotMat_between, "zy").to_array()
         elif i_segment in (10, 13):
@@ -102,7 +123,24 @@ for i_frame in range(nb_frames):
             Q[i_segment * 3: (i_segment + 1) * 3, i_frame] = biorbd.Rotation.toEulerAngles(
                 RotMat_between, "xyz").to_array()
 
+
 Q_corrected = np.unwrap(Q, axis=1)
+
+for i in range(Q_corrected.shape[0]):
+    subtract_pi = False
+    add_pi = False
+    for j in range(Q_corrected.shape[1]):
+        if Q_corrected[i, j] > 5:
+            subtract_pi = True
+            break
+        if Q_corrected[i, j] < -5:
+            add_pi = True
+            break
+    if subtract_pi:
+        Q_corrected[i] = Q_corrected[i] - 2 * np.pi
+    if add_pi:
+        Q_corrected[i] = Q_corrected[i] + 2 * np.pi
+
 Q_degrees = np.degrees(Q)
 Q_complet = np.concatenate((pelv_trans_list[0].T, Q_corrected), axis=0)
 
@@ -111,7 +149,7 @@ ligne_a_supprimer = np.all(Q_complet == 0, axis=1)
 Q_complet_good_DOF = Q_complet[~ligne_a_supprimer, :]
 
 
-for i in range(nb_mat+1):
+for i in range(nb_mat):
     plt.figure(figsize=(5, 3))
     for axis in range(3):
         plt.plot(Q_complet[i*3+axis, :], label=f'{["X", "Y", "Z"][axis]}')
