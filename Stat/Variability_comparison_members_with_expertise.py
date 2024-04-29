66ac3f03319c34375a413695f260ab9017b87eb6
Mathieu <math6060@hotmail.fr>
2024-04-29 14:12:37 -0400

    normality and variance test

--- Stat/Variability_comparison_members_with_expertise.py
+++ Stat/Variability_comparison_members_with_expertise.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from scipy.stats import levene, mannwhitneyu
+from scipy.stats import levene, mannwhitneyu, shapiro, kruskal
 import matplotlib.patches as mpatches
 import numpy as np
 import statsmodels.api as sm
@@ -9,6 +9,7 @@ from statsmodels.formula.api import ols
 from statsmodels.stats.anova import AnovaRM
 from statsmodels.multivariate.manova import MANOVA
 from statsmodels.stats.anova import anova_lm
+from scikit_posthocs import posthoc_dunn
 
 
 def prepare_data(data):
@@ -32,38 +33,59 @@ for file in files:
     data_prepared = data[data['Timing'] != 'Other'].copy()
     data_prepared['Source'] = file.split('/')[-1].replace('results_', '').replace('_position.csv', '')
 
-    # Upper body ANOVA
+    print(f"===== Movement {data_prepared['Source'][0]} is running =====")
+
+    # Check normality and homogeneity of variance for each group
+    measurements = ['upper_body', 'lower_body']
+    for measure in measurements:
+        for expertise in data_prepared['Expertise'].unique():
+            for timing in data_prepared['Timing'].unique():
+                group_data = data_prepared[(data_prepared['Expertise'] == expertise)
+                                           & (data_prepared['Timing'] == timing)][measure]
+                stat, p_value = shapiro(group_data)
+                if p_value < 0.05:
+                    print(f"Normality issue in {timing}, {expertise}, {measure} of {file} (P-value: {p_value:.4f})")
+
+                groups_data = [
+                    data_prepared[(data_prepared['Expertise'] == exp) & (data_prepared['Timing'] == tim)][measure] for
+                    exp in data_prepared['Expertise'].unique() for tim in data_prepared['Timing'].unique()]
+                levene_stat, levene_p = levene(*groups_data)
+                if levene_p < 0.05:
+                    print(f"Variance homogeneity issue across groups in {file} (P-value: {levene_p:.4f})")
+
+
+    # Parametric test ANOVA for upper_body
     modele_upper = ols("upper_body ~ C(Expertise) * C(Timing)", data=data_prepared).fit()
     result_anova_upper = anova_lm(modele_upper, typ=2)
     print(result_anova_upper)
 
-    # Lower body ANOVA
+    # Parametric test ANOVA for lower_body
     modele_lower = ols("lower_body ~ C(Expertise) * C(Timing)", data=data_prepared).fit()
     result_anova_lower = anova_lm(modele_lower, typ=2)
     print(result_anova_lower)
 
-    # MANOVA
+    # Parametric test MANOVA for upper and lower body
     maov = MANOVA.from_formula('upper_body + lower_body ~ C(Expertise) * C(Timing)', data=data_prepared)
     print(maov.mv_test())
 
+
     plt.figure(figsize=(12, 8))
-
-    # Plot for upper body
     upper_plot = sns.pointplot(x='Timing', y='upper_body', hue='Expertise', data=data_prepared,
-                               dodge=0.1, markers=['o', 's'], capsize=0.1, err_kws={'linewidth': 0.3},
-                               palette='Set2', errorbar='sd', label='Upper Body')
+                               dodge=0.1, markers=['o', 's'], capsize=0.1, err_kws={'linewidth': 0.7},
+                               palette='Set2', errorbar='sd')
+    handles, labels = upper_plot.get_legend_handles_labels()
+    plt.legend(handles=handles, labels=labels, title="Expertise")
 
-    # Plot for lower body using the same axes
     lower_plot = sns.pointplot(x='Timing', y='lower_body', hue='Expertise', data=data_prepared,
-                               dodge=0.1, markers=['o', 's'], capsize=0.1, err_kws={'linewidth': 0.3},
-                               palette='Set1', errorbar='sd', label='Lower Body')
+                               dodge=0.1, markers=['o', 's'], capsize=0.1, err_kws={'linewidth': 0.7},
+                               palette='Set1', errorbar='sd')
+    handles, labels = lower_plot.get_legend_handles_labels()
+    plt.legend(handles=handles, labels=labels, title="Expertise")
 
-    # Adding details to the plot
     plt.title('Interaction Between Timing, Expertise, and Body Part on Standard Deviation')
     plt.xlabel('Timing')
     plt.ylabel('Standard Deviation')
 
-    # Adjust legend to show body part and expertise
     handles, labels = plt.gca().get_legend_handles_labels()
     new_labels = []
     exp_levels = list(data_prepared['Expertise'].unique())
@@ -74,3 +96,4 @@ for file in files:
 
     plt.legend(handles, new_labels, title='Expertise and Body Part', bbox_to_anchor=(1, 1), loc='upper left')
     plt.show()
+
--- Stat/Variability_comparison_rotation_with_expertise.py
+++ Stat/Variability_comparison_rotation_with_expertise.py
@@ -1,78 +1,99 @@
 import pandas as pd
 from statsmodels.formula.api import ols
 from statsmodels.stats.anova import anova_lm
+from scipy.stats import shapiro, levene
 import matplotlib.pyplot as plt
 import seaborn as sns
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
-# data = pd.read_csv('/home/lim/Documents/StageMathieu/results_41_rotation.csv')
-# data = pd.read_csv('/home/lim/Documents/StageMathieu/results_42_rotation.csv')
-data = pd.read_csv('/home/lim/Documents/StageMathieu/Tab_result/results_43_rotation.csv')
 
-anova_rot_df = data.pivot_table(index=['ID', 'Expertise'], columns='Timing', values='Std')
-anova_rot_df.reset_index(inplace=True)
+files = [
+    '/home/lim/Documents/StageMathieu/Tab_result/results_41_rotation.csv',
+    '/home/lim/Documents/StageMathieu/Tab_result/results_41o_rotation.csv',
+    '/home/lim/Documents/StageMathieu/Tab_result/results_42_rotation.csv',
+    '/home/lim/Documents/StageMathieu/Tab_result/results_43_rotation.csv'
+]
 
-# Renommer les colonnes pour refléter les nouveaux noms
-anova_rot_df.columns = ['SubjectID', 'Expertise', 'Std_75%', 'Std_Takeoff', 'Std_Landing']
+for file in files:
+    data = pd.read_csv(file)
 
+    anova_rot_df = data.pivot_table(index=['ID', 'Expertise'], columns='Timing', values='Std')
+    anova_rot_df.reset_index(inplace=True)
 
-# Filtrez les données pour chaque comparaison spécifique
-data_75_Landing = data[data['Timing'].isin(['75%', 'Landing'])]
-data_75_takeoff = data[data['Timing'].isin(['75%', 'Takeoff'])]
-data_Landing_takeoff = data[data['Timing'].isin(['Landing', 'Takeoff'])]
+    anova_rot_df.columns = ['SubjectID', 'Expertise', 'Std_75%', 'Std_Takeoff', 'Std_Landing']
 
-# Modèle ANOVA pour 75% vs Landing
-model_75_Landing = ols('Std ~ C(Timing) * C(Expertise)', data=data_75_Landing).fit()
-anova_results_75_Landing = anova_lm(model_75_Landing, typ=2)  # Type II ANOVA
-
-# Modèle ANOVA pour 75% vs Takeoff
-model_75_takeoff = ols('Std ~ C(Timing) * C(Expertise)', data=data_75_takeoff).fit()
-anova_results_75_takeoff = anova_lm(model_75_takeoff, typ=2)  # Type II ANOVA
-
-# Modèle ANOVA pour Landing vs Takeoff
-model_Landing_takeoff = ols('Std ~ C(Timing) * C(Expertise)', data=data_Landing_takeoff).fit()
-anova_results_Landing_takeoff = anova_lm(model_Landing_takeoff, typ=2)  # Type II ANOVA
-
-# Affichage des résultats
-print("ANOVA Results for 75% vs Landing:\n", anova_results_75_Landing)
-print("\nANOVA Results for 75% vs Takeoff:\n", anova_results_75_takeoff)
-print("\nANOVA Results for Landing vs Takeoff:\n", anova_results_Landing_takeoff)
-
-####
-
-# Filtrer les données pour inclure seulement les timings spécifiques
-data_specific = data[data['Timing'].isin(['75%', 'Landing', 'Takeoff'])]
-
-# Modèle ANOVA en tenant compte de 'Timing' et 'Expertise'
-model = ols('Std ~ C(Timing) * C(Expertise)', data=data_specific).fit()
-anova_results = anova_lm(model, typ=2)  # Type II ANOVA
-print("ANOVA Results with Expertise considered:\n", anova_results)
-
-# Création des groupes pour le test de Tukey en convertissant d'abord en chaîne
-data_specific['groups'] = data_specific['Timing'].astype(str) + "_" + data_specific['Expertise'].astype(str)
-
-# Test post-hoc de Tukey HSD
-tukey = pairwise_tukeyhsd(endog=data_specific['Std'], groups=data_specific['groups'], alpha=0.05)
-print(tukey)
-
-data['Timing'] = pd.Categorical(data['Timing'], categories=["Takeoff", "75%", "Landing"], ordered=True)
-data['Expertise'] = pd.Categorical(data['Expertise'], categories=["Elite", "SubElite"], ordered=True)
-
-####
-
-# Création du graphique en différenciant par 'Expertise'
-plt.figure(figsize=(12, 8))
-sns.pointplot(x='Timing', y='Std', hue='Expertise', data=data, dodge=True, markers=['o', 's'],
-              capsize=0.1, err_kws={'linewidth': 0.5}, palette='deep', errorbar='sd')
-
-# Ajout de détails au graphique
-plt.title('Interaction Between Timing and Expertise on Standard Deviation')
-plt.xlabel('Timing')
-plt.ylabel('Standard Deviation')
-plt.legend(title='Expertise')
-
-# Afficher le graphique
-plt.show()
+    data_75_Landing = data[data['Timing'].isin(['75%', 'Landing'])]
+    data_75_takeoff = data[data['Timing'].isin(['75%', 'Takeoff'])]
+    data_Landing_takeoff = data[data['Timing'].isin(['Landing', 'Takeoff'])]
 
 
-#####
+    # Check normality and homogeneity for each group
+    groups = ['75%', 'Landing', 'Takeoff']
+    expertise_levels = data['Expertise'].unique()
+
+    for group in groups:
+        for expertise in expertise_levels:
+            subgroup_data = data[(data['Timing'] == group) & (data['Expertise'] == expertise)]['Std']
+            stat, p_value = shapiro(subgroup_data)
+            if p_value < 0.05:
+                print(f"Normality issue in {group}-{expertise} of {file} (P-value: {p_value:.4f})")
+
+    data_groups = [data[(data['Timing'] == group) & (data['Expertise'] == expertise)]['Std'] for group in groups for
+                   expertise in expertise_levels]
+    levene_stat, levene_p = levene(*data_groups)
+    if levene_p < 0.05:
+        print(f"Variance homogeneity issue across groups in {file} (P-value: {levene_p:.4f})")
+
+    ##
+    # Modèle ANOVA pour 75% vs Landing
+    model_75_Landing = ols('Std ~ C(Timing) * C(Expertise)', data=data_75_Landing).fit()
+    anova_results_75_Landing = anova_lm(model_75_Landing, typ=2)
+
+    # Modèle ANOVA pour 75% vs Takeoff
+    model_75_takeoff = ols('Std ~ C(Timing) * C(Expertise)', data=data_75_takeoff).fit()
+    anova_results_75_takeoff = anova_lm(model_75_takeoff, typ=2)
+
+    # Modèle ANOVA pour Landing vs Takeoff
+    model_Landing_takeoff = ols('Std ~ C(Timing) * C(Expertise)', data=data_Landing_takeoff).fit()
+    anova_results_Landing_takeoff = anova_lm(model_Landing_takeoff, typ=2)
+
+    # Affichage des résultats
+    print("ANOVA Results for 75% vs Landing:\n", anova_results_75_Landing)
+    print("\nANOVA Results for 75% vs Takeoff:\n", anova_results_75_takeoff)
+    print("\nANOVA Results for Landing vs Takeoff:\n", anova_results_Landing_takeoff)
+
+
+    data_specific = data[data['Timing'].isin(['75%', 'Landing', 'Takeoff'])]
+
+    # Modèle ANOVA en tenant compte de 'Timing' et 'Expertise'
+    model = ols('Std ~ C(Timing) * C(Expertise)', data=data_specific).fit()
+    anova_results = anova_lm(model, typ=2)  # Type II ANOVA
+    print("ANOVA Results with Expertise considered:\n", anova_results)
+
+    # Création des groupes pour le test de Tukey en convertissant d'abord en chaîne
+    data_specific['groups'] = data_specific['Timing'].astype(str) + "_" + data_specific['Expertise'].astype(str)
+
+    # Test post-hoc de Tukey HSD
+    tukey = pairwise_tukeyhsd(endog=data_specific['Std'], groups=data_specific['groups'], alpha=0.05)
+    print(tukey)
+
+    data['Timing'] = pd.Categorical(data['Timing'], categories=["Takeoff", "75%", "Landing"], ordered=True)
+    data['Expertise'] = pd.Categorical(data['Expertise'], categories=["Elite", "SubElite"], ordered=True)
+
+    ####
+
+    # Création du graphique en différenciant par 'Expertise'
+    plt.figure(figsize=(12, 8))
+    sns.pointplot(x='Timing', y='Std', hue='Expertise', data=data, dodge=True, markers=['o', 's'],
+                  capsize=0.1, err_kws={'linewidth': 0.5}, palette='deep', errorbar='sd')
+
+    # Ajout de détails au graphique
+    plt.title('Interaction Between Timing and Expertise on Standard Deviation')
+    plt.xlabel('Timing')
+    plt.ylabel('Standard Deviation')
+    plt.legend(title='Expertise')
+
+    # Afficher le graphique
+    plt.show()
+
+
