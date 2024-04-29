5bcf6c1fa207147fc2e025f3c9a38dd76f40872c
Mathieu <math6060@hotmail.fr>
2024-04-29 14:13:33 -0400

    non parametric test

--- Stat/Variability_comparison_rotation.py
+++ Stat/Variability_comparison_rotation.py
@@ -6,28 +6,31 @@ import seaborn as sns
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 from scipy.stats import shapiro, levene
 import os
+from scipy.stats import kruskal
+import scikit_posthocs as sp
+
 
 home_path = "/home/lim/Documents/StageMathieu/Tab_result/"
 
-rotation_files = []  # Renamed variable to avoid conflict
+rotation_files = []
 
 for root, dirs, files in os.walk(home_path):
     for file in files:
         if 'rotation' in file:
             full_path = os.path.join(root, file)
-            rotation_files.append(full_path)  # Use the new list here
+            rotation_files.append(full_path)
 
 
-# DataFrame to store aggregated data for plotting
 all_data = pd.DataFrame()
 
-# Process each file
 for file in rotation_files:
     data = pd.read_csv(file)
     data_specific = data[data['Timing'].isin(['75%', 'Landing', 'Takeoff'])]
     data_specific['Source'] = file.split('/')[-1].replace('results_', '').replace('_rotation.csv', '')  # Clean file ID
 
-    ###
+    print(f"===== Movement {data_specific['Source'][0]} is running =====")
+
+
     # Check normality and homogeneity of variances
     issues = []
     for timing in data_specific['Timing'].unique():
@@ -43,8 +46,9 @@ for file in rotation_files:
 
     if issues:
         print("\n".join(issues))
-    ###
-    # ANOVA without considering 'Expertise'
+
+
+    # Parametric-test ANOVA
     model = ols('Std ~ C(Timing)', data=data_specific).fit()
     anova_results = anova_lm(model, typ=2)
     print(f"ANOVA Results for {data_specific['Source'].iloc[0]}:\n", anova_results)
@@ -53,23 +57,30 @@ for file in rotation_files:
     tukey = pairwise_tukeyhsd(endog=data_specific['Std'], groups=data_specific['Timing'], alpha=0.05)
     print(f"Tukey HSD Results for {data_specific['Source'].iloc[0]}:\n", tukey)
 
+
+    # Non_parametric-test Kruskal_Wallis
+    kruskal_data = [data_specific[data_specific['Timing'] == timing]['Std'].values for timing in
+                    data_specific['Timing'].unique()]
+    kruskal_stat, kruskal_p = kruskal(*kruskal_data)
+    print(f"Kruskal-Wallis Test Results for {data_specific['Source'].iloc[0]} (P-value: {kruskal_p:.4f})")
+
+    if kruskal_p < 0.05:
+        posthoc_results = sp.posthoc_dunn(data_specific, val_col='Std', group_col='Timing', p_adjust='bonferroni')
+        print(f"Post-hoc Dunn's Test Results for {data_specific['Source'].iloc[0]}:\n", posthoc_results)
+
+
     # Append data to the plotting DataFrame
     all_data = pd.concat([all_data, data_specific], ignore_index=True)
 
-# Prepare data for plotting
 all_data['Timing'] = pd.Categorical(all_data['Timing'], categories=["Takeoff", "75%", "Landing"], ordered=True)
 
-# Create the plot
 plt.figure(figsize=(12, 8))
 plot = sns.pointplot(x='Timing', y='Std', hue='Source', data=all_data, dodge=True,
                      capsize=0.1, err_kws={'linewidth': 0.5}, palette='deep', errorbar='sd')
 
-
-# Adding plot details
 plt.title('Standard Deviation Across Different Timings from Multiple Files')
 plt.xlabel('Timing')
 plt.ylabel('Standard Deviation')
 plt.legend(title='File ID', bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
-# Display the plot
 plt.show()
