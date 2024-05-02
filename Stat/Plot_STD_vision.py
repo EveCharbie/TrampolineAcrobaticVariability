233bf5eee0f0d6c6a43fe983f44c3238896ef41f
Mathieu <math6060@hotmail.fr>
2024-05-02 15:16:40 -0400

    plot pelvic SDtotal with gaze on trampoline

--- Stat/Plot_STD_vision.py
+++ Stat/Plot_STD_vision.py
@@ -1,35 +1,87 @@
-import scipy.io
 import pandas as pd
+import scipy.io
+import matplotlib.pyplot as plt
 import numpy as np
-import biorbd
-from scipy.integrate import simpson
-from scipy.interpolate import interp1d
-import pickle
 
 
-data_loaded = scipy.io.loadmat("/home/lim/Documents/StageMathieu/DataTrampo/Xsens_pkl/JoBu/Pos_JC/41/de035f64_0_0-37_556__41__0__eyetracking_metrics.mat")
+def safe_interpolate(x, num_points):
+    # Create an array to store interpolated values, initialized with NaN
+    interpolated_values = np.full(num_points, np.nan)
 
-for key in data_loaded.keys():
-    print(key, type(data_loaded[key]))
+    # Check if all elements are finite, ignore NaNs for interpolation
+    finite_mask = np.isfinite(x)
+    if finite_mask.any():  # Ensure there's at least one finite value to interpolate
+        # Interpolate only finite values
+        valid_x = x[finite_mask]
+        valid_indices = np.linspace(0, 1, len(x))[finite_mask]
 
-if 'wall_index' in data_loaded:
-    print("Wall index is present for participant")
-else:
-    print("Wall index missing for participant")
+        # Perform interpolation over the range with finite values
+        interpolated_valid_values = np.interp(np.linspace(0, 1, num_points), valid_indices, valid_x)
 
-with open("/home/lim/Documents/StageMathieu/DataTrampo/Xsens_pkl/JoBu/42/de035f64_0_0-37_556__42__0__eyetracking_metrics.pkl", "rb") as fichier_pkl:
-    # Charger les données à partir du fichier ".pkl"
-    eye_tracking_metrics = pickle.load(fichier_pkl)
+        # Round interpolated values to the nearest integer
+        rounded_values = np.round(interpolated_valid_values).astype(int)
 
-subject_expertise = eye_tracking_metrics["subject_expertise"]
-subject_name = eye_tracking_metrics["subject_name"]
-move_orientation = eye_tracking_metrics["move_orientation"]
-Xsens_orientation_per_move = eye_tracking_metrics["Xsens_orientation_per_move"]
-Xsens_position_rotated_per_move = eye_tracking_metrics["Xsens_position_rotated_per_move"]
-laterality = eye_tracking_metrics["laterality"]
-wall_index = eye_tracking_metrics["wall_index"]
+        # Place rounded interpolated values back in the full array
+        interpolated_values = rounded_values
+
+    return interpolated_values
+
+file_path = "/home/lim/Documents/StageMathieu/Tab_result/sd_pelvis_and_gaze_orientation.mat"
+data_loaded = scipy.io.loadmat(file_path)
+mean_SD_pelvis_all_subjects_acrobatics = data_loaded["mean_SD_pelvis_all_subjects_acrobatics"]
+movement_to_analyse = data_loaded["movement_to_analyse"]
+wall_index_all_subjects_acrobatics = data_loaded["wall_index_all_subjects_acrobatics"]
+liste_name = data_loaded["liste_name"]
+print(liste_name)
+num_points = 100
+
+for i, mvt in enumerate(movement_to_analyse):
+    up_subject = 0.1
+    custom_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+                     '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',
+                     '#aec7e8', '#ffbb78', '#98df8a', '#ff9896', '#c5b0d5',
+                     '#c49c94', '#dbdb8d']
+
+    colors = custom_colors[:len(wall_index_all_subjects_acrobatics[0][i][0])]
+    max_value = np.max(mean_SD_pelvis_all_subjects_acrobatics[0][i].flatten())
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    for j in range(len(wall_index_all_subjects_acrobatics[0][i][0])):
+        color = colors[j]
+        up_line = max_value + up_subject
+
+        for k in range(len(wall_index_all_subjects_acrobatics[0][i][0][j][0])):
+            data = wall_index_all_subjects_acrobatics[0][i][0][j][0][k]
+            data = pd.DataFrame(data)
+            data_norm = data.apply(lambda x: safe_interpolate(x, num_points))
+
+            # Choose the y-coordinate where the line will be plotted
+            y_line_position = up_line
+
+            # Create an array to hold the y-values for the plot line
+            y_values = np.full(len(data_norm[0]), np.nan)
+
+            # Set y-values to the chosen y-coordinate where the data is zero
+            y_values[data_norm[0] == 0] = y_line_position
+
+            # Plot the line with the color obtained for this iteration of j
+            ax.plot(y_values, '-', color=color, label='Presence of Zero')
+
+            ax.set_title(f"Horizontal Line Indicating Presence of Zeros {mvt}")
+            ax.set_xlabel("Index")
+            ax.set_ylabel("Line Presence (Custom Y Position)")
+            ax.set_xlim(0, len(data_norm[0]))
+            ax.set_ylim(0, max_value+0.45)
+
+            up_line += 0.0005
+        plt.plot(mean_SD_pelvis_all_subjects_acrobatics[0][i][j], label=f'Subject {j + 1}', color=color)
+        up_subject += 0.02
+
+    plt.title(f'Data for {mvt} Acrobatic Movement')
+    plt.xlabel('Data Points')
+    plt.ylabel('Values')
+    plt.show()
 
 
 
-# Enregistrement dans un fichier .mat
-scipy.io.savemat("/home/lim/Documents/de035f64_0_0-37_556__41__0__eyetracking_metrics.mat", mat_data)
+
