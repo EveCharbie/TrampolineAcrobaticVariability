15486fa55bb24ffd74e1247010a2bc79bcebeffb
Mathieu <math6060@hotmail.fr>
2024-05-03 13:54:05 -0400

    all plot same time

--- Stat/Variability_comparison_members_with_expertise.py
+++ Stat/Variability_comparison_members_with_expertise.py
@@ -27,7 +27,18 @@ files = [
     '/home/lim/Documents/StageMathieu/Tab_result/results_43_position.csv'
 ]
 
+# Créez une liste pour stocker tous les axes
+all_axes = []
+
+# Initialiser un compteur pour suivre le nombre d'axes ajoutés
+num_axes = 0
+
+fig, axes = plt.subplots(2, 2, figsize=(16, 12))
+
 for file in files:
+    if num_axes >= 4:
+        break
+
     data = pd.read_csv(file)
     data_prepared = prepare_data(data)
     data_prepared = data[data['Timing'] != 'Other'].copy()
@@ -54,7 +65,6 @@ for file in files:
                 if levene_p < 0.05:
                     print(f"Variance homogeneity issue across groups in {file} (P-value: {levene_p:.4f})")
 
-
     # Parametric test ANOVA for upper_body
     modele_upper = ols("upper_body ~ C(Expertise) * C(Timing)", data=data_prepared).fit()
     result_anova_upper = anova_lm(modele_upper, typ=2)
@@ -69,25 +79,25 @@ for file in files:
     maov = MANOVA.from_formula('upper_body + lower_body ~ C(Expertise) * C(Timing)', data=data_prepared)
     print(maov.mv_test())
 
+    palette_upper = sns.color_palette("Reds", 2)
+    palette_lower = sns.color_palette("Blues", 2)
 
-    plt.figure(figsize=(12, 8))
+    # Plot pour upper_body
     upper_plot = sns.pointplot(x='Timing', y='upper_body', hue='Expertise', data=data_prepared,
                                dodge=0.1, markers=['o', 's'], capsize=0.1, err_kws={'linewidth': 0.7},
-                               palette='Set2', errorbar='sd')
-    handles, labels = upper_plot.get_legend_handles_labels()
-    plt.legend(handles=handles, labels=labels, title="Expertise")
+                               palette=palette_upper, errorbar='sd', ax=axes[num_axes // 2, num_axes % 2])
 
+    # Plot pour lower_body
     lower_plot = sns.pointplot(x='Timing', y='lower_body', hue='Expertise', data=data_prepared,
                                dodge=0.1, markers=['o', 's'], capsize=0.1, err_kws={'linewidth': 0.7},
-                               palette='Set1', errorbar='sd')
-    handles, labels = lower_plot.get_legend_handles_labels()
-    plt.legend(handles=handles, labels=labels, title="Expertise")
+                               palette=palette_lower, errorbar='sd', ax=axes[num_axes // 2, num_axes % 2])
 
-    plt.title(f"Interaction Between Timing, Expertise, and Body Part on Standard Deviation {mvt_name}")
-    plt.xlabel('Timing')
-    plt.ylabel('Standard Deviation')
+    axes[num_axes // 2, num_axes % 2].set_title(
+        f"{mvt_name}")
+    axes[num_axes // 2, num_axes % 2].set_xlabel('Timing')
+    axes[num_axes // 2, num_axes % 2].set_ylabel('Standard Deviation')
 
-    handles, labels = plt.gca().get_legend_handles_labels()
+    handles, labels = axes[num_axes // 2, num_axes % 2].get_legend_handles_labels()
     new_labels = []
     exp_levels = list(data_prepared['Expertise'].unique())
     for i in range(len(labels) // 2):
@@ -95,6 +105,20 @@ for file in files:
     for i in range(len(labels) // 2, len(labels)):
         new_labels.append(f'Lower Body - {exp_levels[i % len(exp_levels)]}')
 
-    plt.legend(handles, new_labels, title='Expertise and Body Part', bbox_to_anchor=(1, 1), loc='upper left')
-plt.show()
+    if num_axes == 0:
+        axes[num_axes // 2, num_axes % 2].legend(handles, new_labels,
+                                                 bbox_to_anchor=(1, 1))
 
+    # Supprimer la légende automatique des autres graphiques
+    if num_axes > 0:
+        axes[num_axes // 2, num_axes % 2].get_legend().remove()
+
+    num_axes += 1
+
+# Supprimer les axes non utilisés
+for i in range(num_axes, 4):
+    fig.delaxes(axes.flatten()[i])
+
+plt.suptitle("Interaction Between Timing and Expertise on Standard Deviation of upper and lower body")
+plt.tight_layout()
+plt.show()
