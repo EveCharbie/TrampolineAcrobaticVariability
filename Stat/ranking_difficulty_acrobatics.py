a6d77d341d9151b171e26b5b8d5f1d2dc3058baf
Mathieu <math6060@hotmail.fr>
2024-05-13 16:08:20 -0400

    75% ranking difficulty

--- Stat/ranking_difficulty_acrobatics.py
+++ Stat/ranking_difficulty_acrobatics.py
@@ -6,6 +6,9 @@ import seaborn as sns
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 from scipy.stats import shapiro, levene
 import os
+import numpy as np
+from scipy.stats import linregress, mannwhitneyu
+
 from scipy.stats import kruskal
 import scikit_posthocs as sp
 from TrampolineAcrobaticVariability.Function.Function_stat import (perform_anova_and_tukey,
@@ -22,41 +25,51 @@ for root, dirs, files in os.walk(home_path):
             full_path = os.path.join(root, file)
             rotation_files.append(full_path)
 
-position_files = []
+files = [
+    '/home/lim/Documents/StageMathieu/Tab_result/results_41_rotation.csv',
+    '/home/lim/Documents/StageMathieu/Tab_result/results_42_rotation.csv',
+    '/home/lim/Documents/StageMathieu/Tab_result/results_43_rotation.csv'
+]
 
-for root, dirs, files in os.walk(home_path):
-    for file in files:
-        if 'position' in file:
-            full_path = os.path.join(root, file)
-            position_files.append(full_path)
+complete_data = pd.DataFrame(columns=['41', '42', '43'])
+
+# complete_data = []
+for file in files:
+    data = pd.read_csv(file)
+    mvt_name = file.split('/')[-1].replace('results_', '').replace('_rotation.csv', '')  # Clean file ID
+    anova_rot_df = data.pivot_table(index=['ID', 'Expertise'], columns='Timing', values='Std')
+    complete_data[mvt_name] = anova_rot_df["75%"]
 
 
-for file_rotation in rotation_files:
+# 1. Boxplots for the different difficulty levels without expertise distinction
+x_boxplot_centers = [0, 1, 2]
 
-    file_position = file_rotation.replace('_rotation.csv', '_position.csv')
+means = [complete_data[col].mean() for col in ['41', '42', '43']]
+slope, intercept, r_value, p_value, std_err = linregress(x_boxplot_centers, means)
 
-    data_rotation = pd.read_csv(file_rotation)
-    data_rotation = data_rotation[data_rotation['Timing'].isin(['75%', 'Landing'])]
-    data_rotation = data_rotation.pivot(index='ID', columns='Timing', values='Std')
-    data_rotation['Delta'] = data_rotation['75%'] - data_rotation['Landing']
-    data_rotation.reset_index(inplace=True)
+x_reg_line = np.array(x_boxplot_centers)
+y_reg_line = slope * x_reg_line + intercept
 
-    data_position = pd.read_csv(file_position)
-    data_position = prepare_data(data_position)
-    data_position = data_position[data_position['Timing'].isin(['75%', 'Landing'])]
-    data_position = data_position.pivot(index='ID', columns='Timing', values=['upper_body', 'lower_body'])
-    data_position['Delta_upper_body'] = data_position['upper_body']['75%'] - data_position['upper_body']['Landing']
-    data_position['Delta_lower_body'] = data_position['lower_body']['75%'] - data_position['lower_body']['Landing']
-    data_position = data_position.drop(columns=['upper_body', 'lower_body'], level=0)
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.boxplot(data=complete_data[['41', '42', '43']], ax=ax, color="skyblue")
+sns.lineplot(x=x_reg_line, y=y_reg_line, ax=ax, color='gray', label='Regression Line', linewidth=1.5)
 
-    data_position.reset_index(inplace=True)
-    data_position.reset_index(drop=True, inplace=True)
+text_str = f'R-squared: {r_value**2:.2f}'
+ax.text(0.02, 0.95, text_str, transform=ax.transAxes, fontsize=10, verticalalignment='top', bbox=dict(boxstyle='round', facecolor='white', alpha=0.5))
 
-    combined_data = pd.merge(data_rotation[['ID', 'Delta']],
-                             data_position[['ID', 'Delta_upper_body', 'Delta_lower_body']], on='ID', how='inner')
+ax.set_title('Boxplot with Regression Line for Different Difficulty Level')
+ax.set_xlabel('Acrobatics by Difficulty Level')
+ax.set_ylabel('75%')
+ax.set_xticks([0, 1, 2])
+ax.set_xticklabels(['41', '42', '43'])
+ax.legend(loc='lower right')
 
-    # Calculer la corrélation de Pearson
-    correlation_results = combined_data.corr()
+plt.show()
 
-    print(correlation_results)
+correlation = complete_data[['41', '42', '43']].corr()
+print(correlation)
+
+
+
+
 
