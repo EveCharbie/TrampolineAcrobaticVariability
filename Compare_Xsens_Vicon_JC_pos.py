14b2cf7c05182910f813c3222c0a51cc18d1cc4f
Mathieu <math6060@hotmail.fr>
2024-06-06 09:58:51 -0400

    discard head

--- Compare_Xsens_Vicon_JC_pos.py
+++ Compare_Xsens_Vicon_JC_pos.py
@@ -45,6 +45,7 @@ name_to_exclude = ['UpperLegR_X', 'UpperLegR_Y', 'UpperLegR_Z',
                    'UpperLegL_X', 'UpperLegL_Y', 'UpperLegL_Z',
                    'UpperArmR_X', 'UpperArmR_Y', 'UpperArmR_Z',
                    'UpperArmL_X', 'UpperArmL_Y', 'UpperArmL_Z',
+                   'Head_X', 'Head_Y', 'Head_Z',
                    ]
 
 columns_to_exclude = []
@@ -54,16 +55,16 @@ for column_name in name_to_exclude:
         columns_to_exclude.append(column_index)
 
 members = [
-    "Pelvic",
-    "Head",
-    "LowerArmR",
-    "HandR",
-    "LowerArmL",
-    "HandL",
-    "LowerLegR",
-    "FootR",
-    "LowerLegL",
-    "FootL",
+    "Pelvis",
+    # "Head",
+    "Elbow R",
+    "Wrist R",
+    "Elbow L",
+    "Wrist L",
+    "Knee R",
+    "Ankle R",
+    "Knee L",
+    "Ankle L",
 ]
 
 ## Plot all try
@@ -88,7 +89,7 @@ for i in range(n_columns_all_axes):
     if i in (0, 1, 2):
         plt.ylabel("Rotation (rad)")
     else:
-        plt.ylabel("Position")
+        plt.ylabel("Position (m)")
 
     if i == 0:
         legend_vicon = mlines.Line2D([], [], color='red', markersize=15, label='Vicon')
@@ -98,6 +99,7 @@ for i in range(n_columns_all_axes):
 
 plt.tight_layout()
 plt.subplots_adjust(top=0.95, hspace=0.5, wspace=0.5)
+plt.close()
 
 ## Plot SD all axes
 
@@ -123,13 +125,17 @@ for i in range(n_columns_all_axes):
     plt.plot(std_xsens, label=f"Xsens - {col_name}", alpha=0.7, linewidth=1, color="blue")
 
     plt.title(f"SD - {col_name}")
-    plt.ylabel("SD")
+
+    if i in (0, 1, 2):
+        plt.ylabel("SD (rad)")
+    else:
+        plt.ylabel("SD (m)")
+
     if i == 0:
         plt.legend(loc='upper right')
 plt.tight_layout()
-
-
 plt.subplots_adjust(top=0.95, hspace=0.5, wspace=0.5)
+plt.close()
 
 std_xsens_all_data = np.stack(std_xsens_all_data)
 std_vicon_all_data = np.stack(std_vicon_all_data)
@@ -140,7 +146,7 @@ std_vicon_all_data = np.stack(std_vicon_all_data)
 result_xsens = np.zeros((len(members), n_points))
 result_vicon = np.zeros((len(members), n_points))
 
-fig, axs = plt.subplots(5, 2, figsize=(13, 17))
+fig, axs = plt.subplots(3, 3, figsize=(13, 17))
 
 for i in range(len(members)):
     start_index = i * 3
@@ -148,8 +154,8 @@ for i in range(len(members)):
     result_xsens[i] = np.mean(std_xsens_all_data[start_index:end_index], axis=0)
     result_vicon[i] = np.mean(std_vicon_all_data[start_index:end_index], axis=0)
 
-    row = i // 2
-    col = i % 2
+    row = i // 3
+    col = i % 3
     # S'assurer que les étiquettes pour la légende sont définies
     line1, = axs[row, col].plot(result_vicon[i], color="red", label="Vicon")
     line2, = axs[row, col].plot(result_xsens[i], color="blue", label="Xsens")
@@ -160,8 +166,83 @@ for i in range(len(members)):
     if i == 0:  # Cette condition peut être changée pour `True` si vous souhaitez la légende sur tous les graphiques
         axs[row, col].legend(loc='upper right')
 
-plt.subplots_adjust(top=0.95, hspace=0.35, wspace=0.2)
+        # Vérifiez si le sous-graphe est dans la dernière ligne
+    if row == 2:  # La dernière ligne a row == 2 dans une grille 3x3
+        axs[row, col].set_xlabel("Time (%)")
+
+
+
+plt.subplots_adjust(top=0.975, bottom=0.09, left=0.05, right=0.995, hspace=0.2, wspace=0.19)
+plt.close()
+
+
+
+fig = plt.figure(figsize=(15, 20))
+gs = fig.add_gridspec(5, 2)  # 5 lignes, 2 colonnes
+y_ticks = np.arange(0, 0.13, 0.03)
+# Premier graphique en haut centré
+ax_top = fig.add_subplot(gs[0, :])
+start_index = 0
+end_index = 3
+result_xsens[0] = np.mean(std_xsens_all_data[start_index:end_index], axis=0)
+result_vicon[0] = np.mean(std_vicon_all_data[start_index:end_index], axis=0)
+ax_top.plot(result_vicon[0], color="red", label="Vicon")
+ax_top.plot(result_xsens[0], color="blue", label="Xsens")
+ax_top.set_title(f'{members[0]}')
+ax_top.set_ylabel('SDtotal (rad)')
+ax_top.legend(loc='upper right')
+
+# Ordre personnalisé pour les graphiques restants
+# custom_order = [1, 3, 2, 4, 5, 7, 6, 8]
+custom_order = [3, 1, 4, 2, 7, 5, 8, 6]
+
+# Graphiques restants en 2 colonnes selon l'ordre personnalisé
+for i, idx in enumerate(custom_order, start=1):
+    start_index = idx * 3
+    end_index = start_index + 3
+    result_xsens[idx] = np.mean(std_xsens_all_data[start_index:end_index], axis=0)
+    result_vicon[idx] = np.mean(std_vicon_all_data[start_index:end_index], axis=0)
+
+    row = (i - 1) // 2 + 1  # Commencer à la deuxième ligne
+    col = (i - 1) % 2
+    ax = fig.add_subplot(gs[row, col])
+    ax.plot(result_vicon[idx], color="red", label="Vicon")
+    ax.plot(result_xsens[idx], color="blue", label="Xsens")
+    ax.set_title(f'{members[idx]}')
+    ax.set_ylim(0, 0.13)  # Limite de l'axe y
+    ax.set_yticks(y_ticks)  # Définir les ticks de l'axe y
+
+    # Ajouter l'étiquette de l'axe x pour la dernière ligne
+    if row == 4:
+        ax.set_xlabel("Time (%)")
+
+        # Ajouter l'étiquette de l'axe x pour la dernière ligne
+    if col == 0:
+        ax.set_ylabel("SDtotal (m)")
+
+    # if col == 1:
+    #     ax.set_yticklabels([])
+
+# Ajout d'un titre global et ajustement des espaces entre les sous-graphiques
+# fig.suptitle('Comparaison des résultats Xsens et Vicon')
+plt.subplots_adjust(top=0.977,
+                    bottom=0.049,
+                    left=0.075,
+                    right=0.99,
+                    hspace=0.329,
+                    wspace=0.143
+                    )
+
 plt.show()
+
+
+
+
+
+
+
+
+
 
 # ## SPM plot
 
