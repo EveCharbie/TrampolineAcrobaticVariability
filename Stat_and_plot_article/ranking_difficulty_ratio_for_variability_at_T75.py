b0f591225ba4bd3b92abb4e705d952bb35d1a8a3
Mathieu <math6060@hotmail.fr>
2024-06-27 14:14:10 -0400

    up

--- Stat_and_plot_article/ranking_difficulty_ratio_for_variability_at_T75.py
+++ Stat_and_plot_article/ranking_difficulty_ratio_for_variability_at_T75.py
@@ -3,40 +3,17 @@ from statsmodels.formula.api import ols
 from statsmodels.stats.anova import anova_lm
 import matplotlib.pyplot as plt
 import seaborn as sns
-from statsmodels.stats.multicomp import pairwise_tukeyhsd
-from scipy.stats import shapiro, levene
 import os
 import numpy as np
-from scipy.stats import linregress, mannwhitneyu
-
-from scipy.stats import kruskal
-import scikit_posthocs as sp
-from TrampolineAcrobaticVariability.Function.Function_stat import (perform_anova_and_tukey,
-                                                                   perform_kruskal_and_dunn,
-                                                                   prepare_data)
+from scipy.stats import linregress
 
 home_path = "/home/lim/Documents/StageMathieu/Tab_result3/"
 
-velocity_at_T75 = {
-    '8-1o': 109,
-    '8-1<': 119,
-    '41': 88,
-    '811<': 127,
-    '41o': 82,
-    '8-3<': 204,
-    '42': 128,
-    '822': 181,
-    '831<': 320,
-    '43': 183,
-}
-
-ratio = [0.25, 0.25, 0.5, 0.5, 0.5, 0.75, 1, 1, 1, 1.5]
 
 order = ['8-1o', '8-1<', '811<', '41', '41o', '8-3<', '42', '831<', '822', '43']
 orderxlabel = ['8-1o', '8-1<', '811<', '41/', '41o', '8-3<', '42/', '831<', '822/', '43/']
 orderxlabeltop = ['0.25', '0.5', '0.75', '1', '1.5']
 
-# x_boxplot_centers = [0, 1, 4, 5, 6, 9, 12, 13, 14, 17]
 ratio = [5, 5, 10, 10, 10, 15, 20, 20, 20, 30]
 x_boxplot_centers = [4.3, 5.7, 8.6, 10, 11.4, 15, 18.6, 20, 21.4, 30]
 x_boxplot_top = [5, 10, 15, 20, 30]
@@ -64,7 +41,6 @@ for file in rotation_files:
     for gymnast_name in data["ID"].unique():
         complete_data.loc[gymnast_name, mvt_name] = np.degrees(anova_rot_df.loc[gymnast_name, "75%"])
 
-# Prepare data for regression using x_boxplot_centers
 all_x_positions = []
 all_values = []
 
@@ -72,25 +48,19 @@ for i, col in enumerate(order):
     all_x_positions.extend([ratio[i]] * complete_data[col].dropna().shape[0])
     all_values.extend(complete_data[col].dropna().values)
 
-# Perform linear regression using x_boxplot_centers and all values
 slope, intercept, r_value, p_value, std_err = linregress(all_x_positions, all_values)
 
-# Create the regression line
 x_reg_line = np.linspace(min(x_boxplot_centers), max(x_boxplot_centers), 100)
 y_reg_line = slope * x_reg_line + intercept
 
-# Plotting
 fig, ax = plt.subplots(figsize=(10, 6))
 sns.boxplot(data=complete_data[order], ax=ax, color="skyblue", positions=x_boxplot_centers)
 sns.lineplot(x=x_reg_line, y=y_reg_line, ax=ax, color='gray', label='Regression Line', linewidth=1.5)
-
-# Add R-squared text<
 
 p_text = "p < 0.001" if p_value < 0.001 else f"p = {p_value:.3f}"
 text_str = f'r = {r_value:.2f}\n{p_text}'
 ax.text(0.02, 0.95, text_str, transform=ax.transAxes, fontsize=10, verticalalignment='top', bbox=dict(boxstyle='round', facecolor='white', alpha=0.5))
 
-# Set labels and title
 ax.set_xlabel('Acrobatics', labelpad=15)
 ax.set_ylabel('Variability of pelvis rotations at T$_{75}$ (deg)')
 ax.set_xticks(x_boxplot_centers)
@@ -98,18 +68,13 @@ ax.set_xticklabels(orderxlabel)
 ax.set_ylim(0, 58)
 ax.legend(loc='lower right')
 
-# Adding the secondary x-axis with different labels
 secax = ax.secondary_xaxis('top')
 secax.set_xticks(x_boxplot_top)
 secax.set_xticklabels(orderxlabeltop)
 secax.set_xlabel('Ratio twists somersaults', labelpad=15)
 
-
-# plt.tight_layout()
 plt.subplots_adjust(top=0.907, bottom=0.098, left=0.056, right=0.995,)
-
 plt.savefig("/home/lim/Documents/StageMathieu/meeting/reg_all.png", dpi=1000)
-
 plt.show()
 
 print(p_value)
